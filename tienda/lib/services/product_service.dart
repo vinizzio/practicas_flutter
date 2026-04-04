0e411e699bf8183ee49a633c8d46ff9d69b2d4f7
Vinizzio <vinizzio@hotmail.com>
2026-04-03 22:36:15 -0500

    parceo

--- tienda/lib/services/product_service.dart
+++ tienda/lib/services/product_service.dart
@@ -1,13 +1,18 @@
 import 'package:dio/dio.dart';
+import 'package:tienda/models/product_model.dart';
 
 class ProductService {
   final dio = Dio();
-  
-  void getProducts () async {
-    final response = await dio.get("https://my-json-server.typicode.com/RicharC293/fake_doctors/products");
-    print(response.data[0]["name"]); 
+
+  void getProducts() async {
+    final response = await dio.get(
+      "https://my-json-server.typicode.com/RicharC293/fake_doctors/products",
+    );
+    //print(response.data[0]["name"]);
+
+    final products = (response.data as List)
+        .map((e) => ProductModel.fromJson(e as Map<String, dynamic>))
+        .toList();
+      print(products.last.price);
   }
-
-
-
 }
